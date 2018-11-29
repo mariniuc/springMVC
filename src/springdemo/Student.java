fef369b41376fe3fd7e47cf952c1d80abe0b6708
mariniuc <36708849+mariniuc@users.noreply.github.com>
2018-11-29 14:46:26 +0200

    Update Student.java

--- src/springdemo/Student.java
+++ src/springdemo/Student.java
@@ -1,11 +1,30 @@
 package springdemo;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 
     private String firstName;
     private String lastName;
+    private String country;
+    private String favouriteLang;
+    private String[] opSys;
+
+    private LinkedHashMap<String, String> countries;
+    private LinkedHashMap<String, String> languages;
 
     public Student() {
+        countries = new LinkedHashMap<>();
+
+        countries.put("BR", "Brasil");
+        countries.put("RO", "Romania");
+        countries.put("ROTW","Rest Of The World");
+
+        languages = new LinkedHashMap<>();
+
+        languages.put("Python","Python");
+        languages.put("Java","Java");
+        languages.put("JavaScript","JavaScript");
     }
 
     public String getFirstName() {
@@ -22,5 +41,37 @@ public class Student {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public LinkedHashMap<String, String> getCountries() {
+        return countries;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavouriteLang() {
+        return favouriteLang;
+    }
+
+    public void setFavouriteLang(String favouriteLang) {
+        this.favouriteLang = favouriteLang;
+    }
+
+    public LinkedHashMap<String, String> getLanguages() {
+        return languages;
+    }
+
+    public String[] getOpSys() {
+        return opSys;
+    }
+
+    public void setOpSys(String[] opSys) {
+        this.opSys = opSys;
     }
 }
